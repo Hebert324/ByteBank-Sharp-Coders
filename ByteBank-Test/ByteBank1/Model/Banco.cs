8c1e77b3493cd7a7cc2b63def78ee1726776cb22
Hebert324 <hebertrocha76@gmail.com>
2023-01-23 19:43:48 -0300

    Adicionando Projeto do ByteBank-Test

---
+++ ByteBank-Test/ByteBank1/Model/Banco.cs
@@ -0,0 +1,40 @@
+﻿namespace ByteBank1.Model {
+    public class Banco {
+
+        // atributo
+        private static List<Conta> _contas;
+        private static Conta? _contaLogada;
+        public readonly string Agencia;
+
+        public Banco(string agencia) {
+            _contas = new List<Conta>();
+            _contaLogada = null;
+            Agencia = agencia;
+        }
+
+        // métodos
+        public static void register() {
+            // TODO apresentar menu de registro
+            Console.WriteLine("Digite o CPF");
+            string cpf = Console.ReadLine();
+
+            Console.WriteLine("Digite o Senha");
+            string senha = Console.ReadLine();
+
+            long id = _contas.Count + 1;
+            _contas.Add(new Conta(id, cpf, senha));
+        }
+
+        public static void login() {
+            Console.WriteLine("Digite o CPF");
+            string cpf = Console.ReadLine();
+            int index = _contas.FindIndex(d => d.Cpf == cpf);
+            Console.WriteLine(_contas[index]);
+        }
+
+        public static void logout() {
+
+        }
+
+    }
+}
---
+++ ByteBank-Test/ByteBank1/Program.cs
@@ -0,0 +1,38 @@
+﻿using ByteBank1.Model;
+using ByteBank1.View;
+
+namespace ByteBank1 {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            List<Conta> contas = new List<Conta>();
+            Console.WriteLine("testing");
+
+            int option;
+
+            do
+            {
+                MenuView.ShowMenu();
+                option = int.Parse(Console.ReadLine());
+                switch (option) 
+                {
+                    case 0:
+                        Console.WriteLine("Saiu do Programa");
+                        break;
+                    case 1:
+                        Banco.register();
+                        break;
+                    case 2:
+                        Banco.login();
+                        break;
+                }
+            } while (option != 0);
+
+            //MenuView.ShowMenu();
+        }
+
+    }
+
+}
